04ed124078db4e6b85f7730718cf21c231c082a2
kelkamel94 <kelkamel94@gmail.com>
2025-11-17 20:23:25 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  
+ function getRandomNumber(maxNumber){
+    return Math.floor(Math.random() * maxNumber);
+  }
+  
+  const arrayCards = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+  const arrayIcon = ["♠","♣","♦","♥"]
+  
+
+  const numberCard = arrayCards[getRandomNumber(arrayCards.length)]
+  const Icon = arrayIcon[getRandomNumber(arrayIcon.length)]
+  
+ 
+  document.getElementById('cardnumber').innerHTML = numberCard
+  document.querySelector('.palo-superior').innerHTML = Icon
+  document.querySelector('.palo-inferior').innerHTML = Icon
+}
+
+
+
+
+
+
+
+
