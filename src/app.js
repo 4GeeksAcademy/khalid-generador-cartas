5a4c71066a7ad982d869c3ed823c4f44e85b44af
kelkamel94 <kelkamel94@gmail.com>
2025-12-03 17:54:18 +0000

    error corregido

--- src/app.js
+++ src/app.js
@@ -5,58 +5,59 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  
-  function getRandomNumber(maxNumber){
+window.onload = function () {
+
+  function getRandomNumber(maxNumber) {
     return Math.floor(Math.random() * maxNumber);
   }
-  
-  const arrayCards = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-  const arrayIcon = ["♠","♣","♦","♥"]
+
+  const arrayCards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  const arrayIcon = ["♠", "♣", "♦", "♥"]
   const historial = [];
-  
+
   function generarCarta() {
     const card = document.querySelector('.card');
-    
+
     card.classList.add('shuffling');
-    
-    setTimeout(function() {
+
+    setTimeout(function () {
       const numberCard = arrayCards[getRandomNumber(arrayCards.length)]
       const Icon = arrayIcon[getRandomNumber(arrayIcon.length)]
-      
+
       document.getElementById('cardnumber').innerText = numberCard
-      document.getElementsByClassName('.palo-superior','.palo-inferior').innerText = Icon
-      
+      document.querySelector('.palo-superior').innerText = Icon;
+      document.querySelector('.palo-inferior').innerText = Icon;
+
       let colorCarta = "negra";
-      if(Icon === "♥" || Icon === "♦") {
+      if (Icon === "♥" || Icon === "♦") {
         card.style.color = "red";
         colorCarta = "roja";
       } else {
         card.style.color = "black";
         colorCarta = "negra";
       }
-      
-      historial.push({numero: numberCard, palo: Icon, color: colorCarta});
+
+      historial.push({ numero: numberCard, palo: Icon, color: colorCarta });
       actualizarHistorial();
-      
+
       card.classList.remove('shuffling');
     }, 300);
   }
-  
+
   function actualizarHistorial() {
     const listaHistorial = document.getElementById('lista-historial');
-    listaHistorial.innerText = ''; 
-    
-    for(let i = historial.length - 1; i >= 0; i--) {
+    listaHistorial.innerText = '';
+
+    for (let i = historial.length - 1; i >= 0; i--) {
       const cartaDiv = document.createElement('div');
       cartaDiv.className = 'carta-historial ' + historial[i].color;
       cartaDiv.innerText = historial[i].numero + historial[i].palo;
       listaHistorial.appendChild(cartaDiv);
     }
   }
-  
+
   generarCarta();
-  
+
   document.getElementById('newCard').addEventListener('click', generarCarta);
-  
+
 }
